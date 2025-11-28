eb49037c25a75a0837862f531bbdd98921e1b8f1
Andrianooo <vladosandrianov@yandex.ru>
2025-11-28 16:48:18 +0300

    добавил модуль flash

---
+++ Core/Inc/FlashUtils.h
@@ -0,0 +1,17 @@
+/*
+ * FlashUtils.h
+ *
+ *  Created on: 28 нояб. 2025 г.
+ *      Author: vlado
+ */
+
+#ifndef INC_FLASHUTILS_H_
+#define INC_FLASHUTILS_H_
+
+#include "main.h"
+
+static FLASH_EraseInitTypeDef EraseInitStruct;
+
+
+
+#endif /* INC_FLASHUTILS_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -29,7 +29,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-static FLASH_EraseInitTypeDef EraseInitStruct;
+
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
