a51d5be049d6506fc75a7a49bdb5ddfa62e8870b
Andrianooo <vladosandrianorx2@gmail.com>
2026-07-14 18:48:48 +0300

    исправил пробный проворот

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2036,6 +2036,7 @@ void parser() {
 		createResponsePacket(0x0F,ACCEPTED__);
 		memcpy(uart3_rx_safe_buffer, uart3_rx_buffer, 6);
 		if (uart3_rx_safe_buffer[2] == 0xFF) { 		// выбрана вертикальная платформа
+			chosen_drv = 1;
 			if (uart3_rx_safe_buffer[1] == 0xFF) { 	// выбрано направление вращения назад
 				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_RESET);
 				driver_dir2 = false;
@@ -2044,7 +2045,8 @@ void parser() {
 				driver_dir2 = true;
 			}
 			startMotorRotation(VERTICAL_, encoder1_data);
-		} else {									// выбрана горизонтальная платформа
+		} else {
+			chosen_drv = 0;		// выбрана горизонтальная платформа
 			if (uart3_rx_safe_buffer[1] == 0xFF) { 	// выбрано направление вращения назад
 				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
 				driver_dir1 = false;
